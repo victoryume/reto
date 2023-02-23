f4a5b0f858a5056fb865a81e3f4266594c30c611
Víctor YuMe <victor.yume4@gmail.com>
2023-02-22 21:51:52 -0500

    Initializr

---
+++ demo/src/main/java/com/retobackend/demo/ServiciorestApplication.java
@@ -0,0 +1,13 @@
+package com.retobackend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciorestApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/retobackend/demo/ServiciorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retobackend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
